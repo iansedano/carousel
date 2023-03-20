4fad98b8865fb2b244cb44664d5e3b3d0cab80e8
Ian Currie <iansedano@gmail.com>
2023-03-20 10:07:01 +0100

    clean index.js

--- src/index.js
+++ src/index.js
@@ -1,10 +1,5 @@
 import { Carousel, InfiniteCarousel } from "./carousel";
 
-function button() {
-  const classList =
-    "rounded-full text-transparent bg-white outline-black outline-[1px] outline w-6 inline-block";
-}
-
 function initCarousels() {
   document.querySelectorAll(".carousel").forEach((element) => {
     new Carousel(element).run(5000);
