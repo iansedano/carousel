9b3ac25d33181bea6d5e9ab2bf56fd0d216be169
Ian Currie <iansedano@gmail.com>
2023-03-20 13:55:38 +0100

    add smooth carousel builder

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
-import { Carousel, InfiniteCarousel } from "./carousel";
+import { Carousel } from "./carousel";
+import { InfiniteCarousel } from "./infinite-carousel";
+import { SmoothCarousel } from "./smooth-carousel";
 
 function initCarousels() {
   document.querySelectorAll(".carousel").forEach((element) => {
@@ -7,6 +9,10 @@ function initCarousels() {
 
   document.querySelectorAll(".infinite-carousel").forEach((element) => {
     new InfiniteCarousel(element).run(5000);
+  });
+
+  document.querySelectorAll(".smooth-carousel").forEach((element) => {
+    new SmoothCarousel(element).run(5000);
   });
 }
 
