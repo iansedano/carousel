b78aad53d7bdc03448e2e812e732cd458083c322
Ian Currie <iansedano@gmail.com>
2023-03-20 09:35:18 +0100

    tween and easing function

---
+++ src/tweens.js
@@ -0,0 +1,26 @@
+function tween(start, end, duration, ease = easeInOutSine) {
+  const startTime = Date.now();
+  const delta = end - start;
+
+  function getValue() {
+    const elapsed = Date.now() - startTime;
+    if (elapsed >= duration) {
+      return end;
+    } else {
+      const fraction = easeInOutSine(elapsed / duration);
+      return delta * fraction + start;
+    }
+  }
+  return getValue;
+}
+
+// https://easings.net/
+
+/**
+ *
+ * @param {*} x progress of animation between 0 and 1
+ * @returns value between 0 and 1
+ */
+function easeInOutSine(x) {
+  return -(Math.cos(Math.PI * x) - 1) / 2;
+}
