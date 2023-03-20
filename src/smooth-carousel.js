4bfee2e9baca1adaa05f8a927bad9c90d9daa8d2
Ian Currie <iansedano@gmail.com>
2023-03-20 13:44:26 +0100

    smooth carousel with custom scroll

---
+++ src/smooth-carousel.js
@@ -0,0 +1,49 @@
+import { scrollWindowX } from "./scroll";
+import { Carousel } from "./carousel";
+
+export class SmoothCarousel extends Carousel {
+  /**
+   * Construct a carousel with a given element.
+   *
+   * Opts out of initializing if no element passed. To allow derived classes to
+   * override initialization.
+   *
+   * @param {Element} window
+   *
+   */
+  constructor(window) {
+    super(window);
+  }
+
+  setupDOM(window) {
+    this.window = window;
+
+    this.window.classList.add(...["relative", "!overflow-x-hidden"]);
+
+    this.carousel = document.createElement("div");
+
+    const carouselClasses = "flex flex-row w-min-fit mx-auto";
+    this.carousel.classList.add(...carouselClasses.split(" "));
+    const slideClasses = ["flex-shrink-0"];
+
+    Array.from(this.window.children).forEach((element) => {
+      element.remove();
+      element.classList.add(...slideClasses);
+      this.carousel.append(element);
+    });
+    this.window.prepend(this.carousel);
+  }
+
+  scrollTo(slideNumber) {
+    scrollWindowX(this.window, this.centers[slideNumber], 3000);
+    // this.window.scrollLeft = this.centers[slideNumber];
+
+    this.buttons.forEach((btn) => {
+      if (btn.dataset.slide == this.currentSlideIndex) {
+        this.styleButton(btn, true);
+      } else {
+        this.styleButton(btn, false);
+      }
+    });
+  }
+}
