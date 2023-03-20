6f76bd6327edc2f5cbbb86c618f333d232d0e5eb
Ian Currie <iansedano@gmail.com>
2023-03-20 10:06:41 +0100

    clean up index.js

--- src/index.js
+++ src/index.js
@@ -7,11 +7,11 @@ function button() {
 
 function initCarousels() {
   document.querySelectorAll(".carousel").forEach((element) => {
-    new Carousel(element).run(1000);
+    new Carousel(element).run(5000);
   });
 
   document.querySelectorAll(".infinite-carousel").forEach((element) => {
-    new InfiniteCarousel(element).run(1000);
+    new InfiniteCarousel(element).run(5000);
   });
 }
 
