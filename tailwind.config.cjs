c13b0560558b8943288057ad738cb36b48e11a3b
Ian Currie <iansedano@gmail.com>
2024-08-30 13:13:29 +0200

    fix tailwind config

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,13 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   // PostCSS doesn't accept glob ** folders
-  content: [
-    "./src/*.{html,mjs,js,ts,css}",
-    "./src/scroll-carousel/*.{html,mjs,js,ts,css}",
-    "./src/translate-carousel/*.{html,mjs,js,ts,css}",
-    "./*.html",
-    "./demos/*.html",
-  ],
+  content: ["./src/**/*.{html,mjs,js,ts,css}", "./*.html", "./demos/*.html"],
   theme: {
     extend: {},
   },
